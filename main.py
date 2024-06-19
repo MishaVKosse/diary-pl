a741600a9d6b15ec9086f982913f1fe025d4060e
MishaVKosse <mishakosse59@gmail.com>
2024-06-19 17:18:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app )
 
 #Zadanie nr 1. Utwórz tabelę DB
-
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    backtitle = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.String(100), nullable=False)
+def __repr__(self):
+        return f'<Card {self.id}>'
 
 
 
